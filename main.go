7879bddfd9ff86e550be4e05a217157b5cb2e8ac
Stephan Schuberth <stephan.schuberth@de-cix.net>
2026-04-28 13:38:39 +0200

    added date to pp.Long

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sjas/sjasgo/bash"
+// 	"fmt"
+// 	"github.com/sjas/sjasgo/bash"
 	"github.com/sjas/sjasgo/pp"
 )
 
 func main(){
-	pp.Long("some test")
+	pp.Long("date test")
+	pp.LongNoDate("nodate test")
 	pp.ShortRed("red")
 	pp.ShortYellow("yellow")
 	pp.ShortGreen("green")
-	fmt.Println(bash.Cmd("hostname -f"))
-	fmt.Println(bash.Cmd("cat /etc/hosts"))
+// 	fmt.Println(bash.Cmd("hostname -f"))
+// 	fmt.Println(bash.Cmd("cat /etc/hosts"))
 }
--- pp/pp.go
+++ pp/pp.go
@@ -5,6 +5,7 @@ import (
 	l "github.com/sirupsen/logrus"
 	"os"
 	"strings"
+	"time"
 	"golang.org/x/term"
 	"github.com/gookit/color"
 )
@@ -37,10 +38,11 @@ pph() {
 }
 */
 
-func pph(fillChar string,colorfg color.Color,colorbg color.Color,maxCharPosition int,input ...string){
+func pph(fillChar string,colorfg color.Color,colorbg color.Color,maxCharPosition int,showTime bool,input ...string){
 	args:=make([]any,len(input))
     for i,v:=range input{args[i]=v}
 	inputString:=strings.TrimSpace(fmt.Sprintln(args...))
+	if showTime{inputString=inputString+" * "+time.Now().Local().Format(time.RFC3339)}
 	prettyPrint:=color.New(color.OpBold,colorfg,colorbg)
 	inputStringLength:=len(inputString)
 	terminalWidth,_,err:=term.GetSize(int(os.Stdout.Fd()));if err!=nil{l.Fatal(err)}
@@ -63,7 +65,8 @@ pps() { pph '+' 80 '42' "$*";}
 ppss() { pph '+' 50 '33' "$*";}
 */
 
-func Long(input ...string){pph("*",color.FgLightWhite,color.BgLightRed,109,input...)}
-func ShortRed(input ...string){pph("+",color.FgWhite,color.BgRed,55,input...)}
-func ShortYellow(input ...string){pph("+",color.FgWhite,color.BgYellow,55,input...)}
-func ShortGreen(input ...string){pph("+",color.FgWhite,color.BgGreen,55,input...)}
+func Long(input ...string){pph("*",color.FgLightWhite,color.BgLightRed,109,true,input...)}
+func LongNoDate(input ...string){pph("*",color.FgLightWhite,color.BgLightRed,109,false,input...)}
+func ShortRed(input ...string){pph("+",color.FgWhite,color.BgRed,55,false,input...)}
+func ShortYellow(input ...string){pph("+",color.FgWhite,color.BgYellow,55,false,input...)}
+func ShortGreen(input ...string){pph("+",color.FgWhite,color.BgGreen,55,false,input...)}
