c94af2c300b19b4eb4d0aca68989084ec70c548f
sjas <stephan.schuberth@de-cix.net>
2026-05-02 00:26:20 +0200

    nokia commands added

--- go.mod
+++ go.mod
@@ -4,11 +4,16 @@ go 1.25.4
 
 require (
 	github.com/gookit/color v1.6.0
+	github.com/scrapli/scrapligo v1.4.0
 	github.com/sirupsen/logrus v1.9.4
 	golang.org/x/term v0.42.0
 )
 
 require (
+	github.com/creack/pty v1.1.23 // indirect
+	github.com/sirikothe/gotextfsm v1.0.1-0.20200816110946-6aa2cfd355e4 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ nokia/nokia.go
@@ -0,0 +1,50 @@
+package nokia
+
+import (
+	l "github.com/sirupsen/logrus"
+	"github.com/sjas/sjasgo/bash"
+	"github.com/scrapli/scrapligo/driver/options"
+    "github.com/scrapli/scrapligo/platform"
+
+)
+
+func runCommandWrapper(host string,cmd string,mdcliEnabled bool)string{
+    user:=bash.CmdToStringWithoutFullEnvironment("getuser .aduser")
+	pass:=bash.CmdToStringWithoutFullEnvironment("getpass .adpass")
+	var osVersion string
+	if mdcliEnabled{
+		osVersion="nokia_sros"
+	}else{
+		osVersion="nokia_sros_classic"
+	}
+    p,err:=platform.NewPlatform(
+        osVersion,
+        host,
+        options.WithAuthNoStrictKey(),
+        options.WithAuthUsername(user),
+        options.WithAuthPassword(pass),
+        options.WithTransportType("system"),
+    );if err!=nil{l.Fatal(err)}
+    d,err:=p.GetNetworkDriver();if err!=nil{l.Fatal(err)}
+    err=d.Open();if err!=nil{l.Error(err)}
+    defer d.Close()
+    prompt,err:=d.Channel.GetPrompt();if err!=nil{l.Fatal(err)}
+    l.Info("found prompt: ",string(prompt))
+
+    res_bytes,err:=d.Channel.SendInput(cmd)
+    if err!=nil{l.Fatal(err)}
+    res:=string(res_bytes)
+	l.Info("ran commmand "+cmd+" on host "+host)
+    l.Debug(res)
+	return res
+}
+
+func Classic(host string,cmd string)string{
+	mdcliEnabled:=false
+	return runCommandWrapper(host,cmd,mdcliEnabled)
+}
+
+func Mdcli(host string,cmd string)string{
+	mdcliEnabled:=true
+	return runCommandWrapper(host,cmd,mdcliEnabled)
+}
