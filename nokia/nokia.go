e546c8a6ab8b360a6fba4700c7008e40c03afaf4
sjas <stephan.schuberth@de-cix.net>
2026-05-06 14:27:55 +0200

    added morenokia debugging

--- nokia/nokia.go
+++ nokia/nokia.go
@@ -53,8 +53,10 @@ func Classic(host string,cmd ...string)string{
 func runCommandWorker(host string,mdcliEnabled bool,wg *sync.WaitGroup,c chan map[string]string,cmd ...string){
 	defer wg.Done()
 	res:=make(map[string]string)
+	l.Debug("started worker")
 	if mdcliEnabled{res[host]=Mdcli(host,cmd...)
 	}else{res[host]=Classic(host,cmd...)}
+	l.Debug("finished worker")
 	c<-res
 }
 
@@ -73,7 +75,10 @@ func RunCommandOnHostList(hostlist []string,mdcliEnabled bool,cmd ...string)map[
 		l.Debug("trigger command worker ",i)
 		runCommandWorker(i,mdcliEnabled,&wg,c,cmd...)
 	}
+	l.Debug("all workers started")
 	go func(){wg.Wait();close(c)}()
+	l.Debug("waitgroup finished and channel closed, accumulating result maps")
 	for i:=range c{maps.Copy(res,i)}
+	l.Debug("result maps accumulated")
 	return res
 }
