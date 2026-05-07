1fb7c6d6f5b87c5252a0fbc97765feee32f8fe60
sjas <stephan.schuberth@de-cix.net>
2026-05-07 12:09:11 +0200

    dont panic on broken ssh connects

--- nokia/nokia.go
+++ nokia/nokia.go
@@ -29,13 +29,13 @@ func runCommandWrapper(host string,mdcliEnabled bool,cmd ...string)string{
         options.WithTransportType("system"),
     );if err!=nil{panic(err)}
     d,err:=p.GetNetworkDriver();if err!=nil{panic(err)}
-    err=d.Open();if err!=nil{panic(err)}
+    err=d.Open();if err!=nil{l.Error(err)}
     defer d.Close()
     _,err=d.Channel.GetPrompt();if err!=nil{panic(err)}
 
 	var fullResBytes []byte
 	for _,i:=range cmd{
-		resBytes,err:=d.Channel.SendInput(i);if err!=nil{panic(err)}
+		resBytes,err:=d.Channel.SendInput(i);if err!=nil{l.Error(err)}
 		fullResBytes=append(fullResBytes,resBytes...)
 		fullResBytes=append(fullResBytes,'\n')
 		fullResBytes=append(fullResBytes,'\n')
